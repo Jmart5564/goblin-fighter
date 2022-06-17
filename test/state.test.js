8a837c38e86884fcc3b028edaa768b7f943e523e
Jmart5564 <Jmart5564@gmail.com>
2022-06-17 13:07:18 -0700

    passing test

--- test/state.test.js
+++ test/state.test.js
@@ -10,12 +10,21 @@ const test = QUnit.test;
 
 test('the first state test...', (expect) => {
     // what is the initial expected state?
-
+    const expected = {
+        defeated: 0,
+        hp: 10,  
+        damage: [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2],
+        enemyMessage: '',
+        playerMessage: '',
+        enemies: [
+            { name: 'Poe', health: 3 },
+        ],
+    };
     // use the action
 
     // what should the state be now?
 
 
     // remove this line when starting your test
-    expect.deepEqual(state, {});
+    expect.deepEqual(state, expected);
 });
